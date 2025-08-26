221a392ee1b529f2a99ea27add7e1b15dbedd081
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-26 12:20:20 +0600

    navar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
-import { Outlet } from 'react-router'
+import { Link, Outlet } from 'react-router'
+import { useGetUsersQuery } from './redux/features/users/userApi';
 
 function App() {
- 
 
+  const { data, error, isLoading } = useGetUsersQuery();
+  console.log(data)
   return (
     <>
+    <nav className='bg-blue-400 text-white p-4 '>
+        <ul className='flex flex-wrap space-x-4 items-center justify-end gap-14'>
+           <li><Link to="/" className='bg-blue-700 px-6 py-2 rounded-lg'>Home</Link></li>
+           <li><Link to="/user" className='bg-blue-700 px-6 py-2 rounded-lg'>Users</Link></li>
+           <li><Link to="/about" className='bg-blue-700 px-6 py-2 rounded-lg'>About</Link></li>
+           <li><Link to="/contact" className='bg-blue-700 px-6 py-2 rounded-lg'>Contact</Link></li>
+        </ul>
+    </nav>
       <main>
         <Outlet/>
       </main>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import Users from "./components/Users.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
     <Routes>
       <Route element={<App />}>
         <Route path="/" element={<div>RTK Query Advanced Concept</div>} />
+        <Route path="/user" element={<Users/>} />
       </Route>
     </Routes>
   </BrowserRouter>
