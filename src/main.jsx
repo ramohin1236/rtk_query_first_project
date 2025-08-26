bb7d39a1b6691e946e4035e8065ae071274c0f1f
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-26 11:20:19 +0600

    provider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
     <Routes>
       <Route element={<App />}>
         <Route path="/" element={<div>RTK Query Advanced Concept</div>} />
       </Route>
     </Routes>
   </BrowserRouter>
+  </Provider>
 );
