fd80404bcd1aca4f8932abcf34d13d89836b10d7
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-26 11:44:13 +0600

    user api find

--- src/redux/features/users/userApi.js
+++ src/redux/features/users/userApi.js
@@ -1,8 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const userApi = createApi({
-     reducerPath: 'userApi',
-     baseQuery: fetchBaseQuery({
-         baseUrl: 'https://pokeapi.co/api/v2/'
-     }),
-})
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000",
+  }),
+  endpoints: (builder) => ({
+    //   get all users
+    getUsers: builder.query({
+      query: () => "/users",
+    }),
+  }),
+});
+
+
+export const { useGetUsersQuery } = userApi;
+
+export default userApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userApi from './features/users/userApi'
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 })
 
 export default store
