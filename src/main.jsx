32fe0f3675b302934445071152f21620d3a572e0
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-26 11:13:45 +0600

    basic project setup

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<div>RTK Query Advanced Concept</div>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
