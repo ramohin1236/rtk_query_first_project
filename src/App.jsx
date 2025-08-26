8729dd656fd7c36faf2efb942d29a717c6aa34cb
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-26 12:26:23 +0600

    routes complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
         <ul className='flex flex-wrap space-x-4 items-center justify-end gap-14'>
            <li><Link to="/" className='bg-blue-700 px-6 py-2 rounded-lg'>Home</Link></li>
            <li><Link to="/user" className='bg-blue-700 px-6 py-2 rounded-lg'>Users</Link></li>
-           <li><Link to="/about" className='bg-blue-700 px-6 py-2 rounded-lg'>About</Link></li>
-           <li><Link to="/contact" className='bg-blue-700 px-6 py-2 rounded-lg'>Contact</Link></li>
+           <li><Link to="/add-user" className='bg-blue-700 px-6 py-2 rounded-lg'>Add User</Link></li>
+           <li><Link to="/edit-user/:id" className='bg-blue-700 px-6 py-2 rounded-lg'>Edit User</Link></li>
         </ul>
     </nav>
       <main>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import Users from "./components/Users.jsx";
+import AddUser from "./components/AddUser.jsx";
+import EditUser from "./components/EditUser.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
@@ -13,6 +15,8 @@ createRoot(document.getElementById("root")).render(
       <Route element={<App />}>
         <Route path="/" element={<div>RTK Query Advanced Concept</div>} />
         <Route path="/user" element={<Users/>} />
+        <Route path="/add-user" element={<AddUser/>} />
+        <Route path="/edit-user/:id" element={<EditUser/>} />
       </Route>
     </Routes>
   </BrowserRouter>
